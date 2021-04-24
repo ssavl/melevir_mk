50f530ad61a0896fa961c195aabfcaf3f4be7504
Savelii Stepura <get-moscow@yandex.ru>
2021-04-24 12:27:25 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -3,16 +3,27 @@
 
 
 api_urls = [
-    path('companies/', comp_view.CompanyViewSet.as_view({
-        'get': 'list', 'post': 'create'
-    }), name='companies-list'),
-    path('companies/<int:company_id>/', comp_view.CompanyViewSet.as_view({
+    path('users/', UsersViewSet.as_view({
         'get': 'retrieve',  'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'
-    }), name='companies-detail'),
+    }), name='users-list'),
+    
+    path('companies/<int:company_id>/', comp_view.UsersViewSet.as_view({
+        'get': 'retrieve',  'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'
+    }), name='user-detail'),
 
-    path('companies/<int:company_id>/ssds/', comp_view.SubdivisionViewSet.as_view({
-        'get': 'list', 'post': 'create'
-    }), name='subdivisions-list'),
-    path('companies/<int:company_id>/ssds/<int:subdivision_id>/', comp_view.SubdivisionViewSet.as_view({
+    path('add/', AddViewSet.as_view({
+       'get': 'retrieve',  'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'
+    }), name='ad-list'),
+    
+    path('add/<int:company_id>/', AdViewSet.as_view({
         'get': 'retrieve',  'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'
-    }), name='subdivisions-detail'),
+    }), name='ad-detail'),
+    
+    path('add/<int:company_id>/', AdViewSet.as_view({
+        'get': 'retrieve',  'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'
+    }), name='ad-detail'),
+    
+    path('add/<int:company_id>/', AdViewSet.as_view({
+        'get': 'retrieve',  'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'
+    }), name='ad-detail'),
+    
