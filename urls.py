118bc01bdf267f4a492ead8d4cf084833da3a2bf
Savelii Stepura <get-moscow@yandex.ru>
2021-04-24 12:09:51 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,1 +1,18 @@
 
+
+
+
+api_urls = [
+    path('companies/', comp_view.CompanyViewSet.as_view({
+        'get': 'list', 'post': 'create'
+    }), name='companies-list'),
+    path('companies/<int:company_id>/', comp_view.CompanyViewSet.as_view({
+        'get': 'retrieve',  'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'
+    }), name='companies-detail'),
+
+    path('companies/<int:company_id>/ssds/', comp_view.SubdivisionViewSet.as_view({
+        'get': 'list', 'post': 'create'
+    }), name='subdivisions-list'),
+    path('companies/<int:company_id>/ssds/<int:subdivision_id>/', comp_view.SubdivisionViewSet.as_view({
+        'get': 'retrieve',  'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'
+    }), name='subdivisions-detail'),
